31e82940c81376f836f462320cae113281376f64
william <c.william.robertson@gmail.com>
2017-10-02 12:45:22 +0200

    indentation was made on the html returned by getpage

--- view/HTMLPage.php
+++ view/HTMLPage.php
@@ -11,15 +11,16 @@ class HTMLPage {
 	 * @return String HTML
 	 */
 	public function getPage($title, $body) {
-		return '<!DOCTYPE HTML SYSTEM>
-<html>
-	<head>
-		<title>' . $title . '</title>
-		<meta http-equiv=\'content-type\' content=\'text/html; charset=utf8\'>
-	</head>
-	<body>
-		' . $body . '
-	</body>
-</html>';
+		return 
+		'<!DOCTYPE HTML SYSTEM>
+			<html>
+				<head>
+					<title>' . $title . '</title>
+					<meta http-equiv=\'content-type\' content=\'text/html; charset=utf8\'>
+				</head>
+				<body>
+					' . $body . '
+				</body>
+			</html>';
 	}
 }
